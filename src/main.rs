491eaccef22f736c8cbcf6d9232c453bf1c30506
Alexander Kudrevatykh <alexander@kudrevatykh.com>
2020-01-23 21:53:40 +0300

    update to latest rust and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,46 @@
 #![deny(warnings)]
-extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
-extern crate tokio_core;
+extern crate tokio;
 
-use futures::{Future, Stream};
-
-use hyper::{Body, Chunk, Client, Get, StatusCode};
+use hyper::{Server, Body, Client, Method, StatusCode, Request, Response};
+use hyper::service::{make_service_fn, service_fn};
 use hyper_tls::HttpsConnector;
-use hyper::error::Error;
-use hyper::header::ContentLength;
-use hyper::server::{Http, Service, Request, Response};
+use hyper::client::HttpConnector;
 
 use std::env;
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
 
 static NOTFOUND: &[u8] = b"Not Found";
 static OK: &[u8] = b"OK";
 static REQ_ERR: &[u8] = b"request error";
 
-pub type ResponseStream = Box<Stream<Item=Chunk, Error=Error>>;
 
-struct DynDnsProxy(tokio_core::reactor::Handle);
-
-impl Service for DynDnsProxy {
-    type Request = Request;
-    type Response = Response<ResponseStream>;
-    type Error = hyper::Error;
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
-
-    fn call(&self, req: Request) -> Self::Future {
-        match (req.method(), req.path()) {
-            (&Get, "/ip") => {
-                let urls = get_urls() ;
-                let query = req.query().unwrap_or("");
+async fn handle_request (
+    req: Request<Body>,
+    client: Client<HttpsConnector<HttpConnector>>,
+    urls: Vec<String>,
+) -> Result<Response<Body>> {
+        match (req.method(), req.uri().path()) {
+            (&Method::GET, "/ip") => {
+                let query = req.uri().query().unwrap_or("");
                 let ip = query.split('&').find(|x| x.starts_with("myip=")).unwrap_or("").replace("myip=", "");
                 // Run a web query against the web api below
-                let client = Client::configure().connector(HttpsConnector::new(4, &self.0).unwrap()).build(&self.0);
-                let mut future : Box<Future<Item=_,Error=_>> = Box::new(futures::future::ok(Response::new()));
                 for url in &urls {
-                    let req = Request::new(Get, url.replace("$ip", &ip).parse().unwrap());
-                    let web_res_future : Box<Future<Item=Response,Error=_>> = Box::new(client.request(req));
-                    future = Box::new(future.and_then(|r| if r.status().is_success() {web_res_future} else {Box::new(futures::future::err(Error::Status))}));
+                    let resp = client.get(url.replace("$ip", &ip).parse()?).await?;
+                    if !resp.status().is_success() {
+                        return Ok(Response::builder().status(StatusCode::BAD_GATEWAY).body(Body::from(REQ_ERR)).unwrap())
+                    }
                 }
-				Box::new(future.map(|_| {
-                	let body: ResponseStream = Box::new(Body::from(OK));
-                    Response::new()
-                        .with_body(body)
-                        .with_header(ContentLength(OK.len() as u64))
-                }).or_else(|_| {
-                	let body: ResponseStream = Box::new(Body::from(REQ_ERR));
-                    futures::future::ok(Response::new()
-                        .with_body(body)
-                        .with_status(StatusCode::BadGateway)
-                        .with_header(ContentLength(REQ_ERR.len() as u64)))
-                }))
+                Ok(Response::builder().body(Body::from(OK)).unwrap())
             },
             _ => {
-                let body: ResponseStream = Box::new(Body::from(NOTFOUND));
-                Box::new(futures::future::ok(Response::new()
-                                             .with_status(StatusCode::NotFound)
-                                             .with_header(ContentLength(NOTFOUND.len() as u64))
-                                             .with_body(body)))
+                Ok(Response::builder().body(Body::from(NOTFOUND)).unwrap())
             }
         }
     }
-
-}
 
 fn get_urls() -> Vec<String> {
 	let mut urls = Vec::new();
@@ -80,22 +55,25 @@ fn get_urls() -> Vec<String> {
 	return urls;
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let addr = "0.0.0.0:1337".parse().unwrap();
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let new_service = make_service_fn(move |_| {
+        // Move a clone of `client` into the `service_fn`.
+        let client = client.clone();
+        let urls = get_urls();
+        async {
+            Ok::<_, GenericError>(service_fn(move |req| {
+                // Clone again to ensure that client outlives this closure.
+                handle_request(req, client.to_owned(), urls.to_owned())
+            }))
+        }
+    });
 
-    let mut core = tokio_core::reactor::Core::new().unwrap();
-    let handle = core.handle();
-    let client_handle = core.handle();
-
-    let serve = Http::new().serve_addr_handle(&addr, &handle, move || Ok(DynDnsProxy(client_handle.clone()))).unwrap();
-    println!("Listening on http://{} with 1 thread.", serve.incoming_ref().local_addr());
-
-    let h2 = handle.clone();
-    handle.spawn(serve.for_each(move |conn| {
-        h2.spawn(conn.map(|_| ()).map_err(|err| println!("serve error: {:?}", err)));
-        Ok(())
-    }).map_err(|_| ()));
-
-    core.run(futures::future::empty::<(), ()>()).unwrap();
+    let server = Server::bind(&addr).serve(new_service);
+    println!("Listening on http://{} with 1 thread.", addr);
+    server.await?;
+    Ok(())
 }
